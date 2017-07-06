f6c64954769d6a2fc7bde64d37a56020b09a2c06
ZXChadd <zhexianchadd@gmail.com>
2017-07-06 14:29:41 +0800

    feature omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
   has_many :following, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :followers, class_name: 'Relationship', foreign_key: 'followee_id', dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :notes, dependent: :destroy
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      # user.name = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
 
 end
