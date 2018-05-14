d5bf6452f562126dd748d25c816cbdc9516f2ae9
ZXChadd <zhexianchadd@gmail.com>
2018-05-14 12:38:26 +0800

    Resolve dependencies

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+
+  validates :body, presence: true
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Note, type: :model do
   it { should belong_to(:user) }
   it { should have_many(:likes) }
+
+  describe 'note should have a body' do
+    it { should validate_presence_of(:body) }
+  end
 end
